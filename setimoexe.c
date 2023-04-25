90f268b796629a6550fb34b04cdb05fbde43458a
meduardor <meduardo.r@tutanota.com>
2023-04-25 18:46:05 -0300

    new files ":)" de exercicios

--- setimoexe.c
+++ setimoexe.c
@@ -1,5 +1,8 @@
+
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+
 
 
 
@@ -13,19 +16,24 @@ int main()
 
   printf("Digite o dia do aniversario:\n");
   scanf("%d", &dia);
+  sprintf(dia_s,"%d",dia);
 
   printf("Digite o Mês do seu aniversario:\n");
   scanf("%d", &mes);
+  //FIXME: Retorno da string esta acresntando um valor no 0.
+  sprintf(mes_s,"%d", mes);
 
   printf("Digite o Ano do seu aniversario:\n");
   scanf("%d", &ano);
 
-
-  itoa(dia, dia_s, 2);
+  // concatenação dos elementos 
+  strcat(dia_s, mes_s);
 
   
+  int valor = atoi(dia_s);
+  int result = valor + ano;
 
-  printf("test %d, %d, %d", dia, mes, ano);
-
+  printf("%d, %d \n", valor, result);
+  
   return 0;
 }
